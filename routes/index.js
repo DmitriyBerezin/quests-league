10b60b610217dd1812173e895c4d0cd11a7f3534
DmitriyBerezin <dberezin@mail.ru>
2015-04-19 12:24:39 +0300

    Amazon db test

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool  = mysql.createPool({
+	connectionLimit : 10,
+	host            : 'quest2.cp0uujwgrxiz.eu-west-1.rds.amazonaws.com',
+	user            : 'root',
+	password        : 'rootroot'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,7 +21,31 @@ router.get('/', function(req, res, next) {
   	console.log(req.session);
   }
 
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  getTestDataFromDb(function() {
+  	res.render('index', { title: 'Express' });
+  })
 });
+
+function getTestDataFromDb(callback) {
+	pool.getConnection(function(err, connection) {
+		console.log(err);
+
+		// Use the connection
+		connection.query( 'call quest.test_proc();', function(err, rows) {
+			if (err) {
+				console.log('Error:', err);
+			}
+
+			console.log('Db data are:', rows);
+			// And done with the connection.
+			connection.release();
+
+			// Don't use the connection here, it has been returned to the pool.
+
+			callback();
+		});
+	});
+}
 
 module.exports = router;
