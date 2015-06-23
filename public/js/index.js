fe4df7dd814935f89b80f3a3d46e3f4d2eb8e19c
Dmitriy Berezin <berezin.d@frsd.ru>
2015-06-23 11:19:14 +0300

    Finish markup
    https://github.com/DmitriyBerezin/quests-league/issues/6

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,40 @@
 $(function() {
-	$('.selectpicker').selectpicker();
+	var $formCompany = $('.form-company'),
+		$formQuest = $('.form-quest');
+
+	$formCompany.validate({
+		errorElement: 'div',
+		errorClass: 'error'
+	});
+	$formCompany.ajaxForm({
+		success: onFormCompanySuccess,
+		error: onFormCompanyError
+	});
+
+	$formQuest.validate({
+		errorElement: 'div',
+		errorClass: 'error',
+		errorPlacement: function (error, element) {
+			if (element.hasClass('selectpicker')) {
+				element.next('.bootstrap-select').after(error);
+			}
+			else {
+				element.after(error);
+			}
+		}
+	});
+	// http://stackoverflow.com/questions/21018970/bootstrap-select-plugin-not-work-with-jquery-validation
+	$formQuest.data('validator').settings.ignore = '';
+
+
+
+	$('.btn-company-create').click(onConpanyCreateClick);
+
+	function onFormCompanySuccess(responseText, statusText, xhr) {
+
+	}
+
+	function onFormCompanyError(error) {
+
+	}
 });
