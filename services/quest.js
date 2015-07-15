98f34276d5f4d1653918f84ab67e442b454c87fb
Dmitriy Berezin <berezin.d@frsd.ru>
2015-07-15 18:12:21 +0300

    Other company quests images
    https://github.com/DmitriyBerezin/quests-league/issues/10

--- services/quest.js
+++ services/quest.js
@@ -1,37 +1,46 @@
 var util = require('util'),
+	async = require('async'),
 	db = require('./database'),
 	s3 = require('./aws-s3'),
 	admin = require('./admin');
 
 function getQuest(id, done) {
 	var query = util.format('call quests.pQuestGet1(%s)', id || null),
-		data;
+		res;
 
 	db.execQuery(query, function(err, rows, fields) {
+		var filesFunc = {};
+
 		if (err) {
 			return done(err);
 		}
 
-		data = rows[0].length > 0 ? rows[0][0] : {};
-		data.tags = rows[1];
-		data.quests = rows[2];
-		data.imgs = [];
+		res = rows[0].length > 0 ? rows[0][0] : {};
+		res.tags = rows[1];
+		res.quests = rows[2];
+		res.imgs = [];
 
-		if (id) {
-			admin.getQuestFiles(id, function(err, imgs) {
-				if (err) {
-					return done(err);
-				}
+		filesFunc[id] = admin.getQuestFiles.bind(null, id);
+		for (var i = 0; i < res.quests.length; ++i) {
+			filesFunc[res.quests[i].id] = admin.getQuestFiles.bind(null, res.quests[i].id);
+		}
 
-				data.imgs = imgs;
-				return done(null, data);
-			})
-		}
-		else {
-			return done(null, data);
-		}
+		async.parallel(filesFunc, function(err, data) {
+			if (err) {
+				return done(err);
+			}
+
+			res.imgs = data[id];
+			for (var i = 0; i < res.quests.length; ++i) {
+				res.quests[i].img = data[res.quests[i].id].length > 0 ?
+					data[res.quests[i].id][0] : null;
+			}
+
+			return done(null, res);
+		});
 	});
 }
+
 module.exports = {
 	getQuest: getQuest
 };
