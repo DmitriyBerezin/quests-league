763568b9db93d4802a62a7ce1678bd531287124e
Dmitriy Berezin <berezin.d@frsd.ru>
2015-07-28 17:54:28 +0300

    Display some specified quests if the search query is empty
    https://github.com/DmitriyBerezin/quests-league/issues/22

--- sql/grant-access.sql
+++ sql/grant-access.sql
@@ -14,8 +14,11 @@ grant execute on procedure pCountryCities to 'moderator';
 grant execute on procedure pQuestGet1 to 'moderator';
 grant execute on procedure pQuestGetIdBySefName to 'moderator';
 
+update tquest q set q.top = 1 where id = 8;
 
-insert into tcountry value('Россия');
+
+insert into tcountry(name) value('Россия');
+insert into tcity(name, coutry_id) value('Москва', 1);
 update tcity set country_id = 1;
 -- Выполняем перед fk constraint
 update tquest set country_id = 1;
